6f0b50c788ae9fb9ae697c224661d69495f86460
Shubham Sharma <Shubham101021@gmail.com>
2023-05-08 10:25:23 +0530

    some change

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,28 +2,29 @@ import Breadcrumbs from "./Breadcrumbs";
 import styles from "./Hero.module.css";
 import RecentResult from "./RecentResult";
 
-const Hero = ({ title, content , MyBackgroundImage }) => {
+const Hero = ({ title, content, MyBackgroundImage }) => {
   return (
     <>
-    <div className={styles.hero}>
-      <div className={styles.empoweringYourOnlinePresencParent} style={{backgroundImage:`url(${MyBackgroundImage})`}}>
-        <div className={styles.empoweringYourOnlineContainer}>
-          <div className="col-6">
-            <p  className={styles.empoweringYour + "my-3"}>{title}</p>
-            <div className={styles.joinTheTechrowthContainer }>
-              <p className={styles.empoweringYour + "my-3"} >
-                <span>{content}</span>
-              </p>
+      <div className={styles.hero}>
+        <div
+          className={styles.empoweringYourOnlinePresencParent}
+          style={{ backgroundImage: `url(${MyBackgroundImage})` }}
+        >
+          <div className={styles.empoweringYourOnlineContainer}>
+            <div className="col-6">
+              <p className={styles.empoweringYour + "my-3"}>{title}</p>
+              <div className={styles.joinTheTechrowthContainer}>
+                <p className={styles.empoweringYour + "my-3"}>
+                  <span>{content}</span>
+                </p>
+              </div>
+              <button className={styles.cta}>
+                <div className={styles.talkToOur}>Talk to our strategist</div>
+              </button>
             </div>
-            <button className={styles.cta}>
-              <div className={styles.talkToOur}>Talk to our strategist</div>
-            </button>
           </div>
         </div>
       </div>
-    </div>
-    <RecentResult/>
-    <Breadcrumbs/>
     </>
   );
 };
--- src/pages/IntegrationPage.js
+++ src/pages/IntegrationPage.js
@@ -1,9 +1,87 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import styles from "../components/OneStopSolution1.module.css";
+import Hero from "../components/Hero";
+import heroBg from "../assets/rectangle-216@2x.png";
+import ToolsSlider from "../components/ToolsSlider";
+import ContactUsBar from "../components/ContactUsBar";
+import ContactForm1 from "../components/ContactForm1";
+import TrustedByBrands from "../components/TrustedByBrands";
+import Breadcrumbs from "../components/Breadcrumbs";
+import RecentResult from "../components/RecentResult";
 
 const IntegrationPage = () => {
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, []);
+  const HeroTitel = "Empowering your Online presence, Driving Actual Results";
+  const HeroContent =
+    "Join the Techrowth Revolution Experience Exceptional Results and elevate your business to new levels with our result-oriented strategy framework BISCCBOR!";
+  const RecentText =
+    "Interested in Getting In ? Discover how Techrowth can help grow your Business.";
   return (
-    <div>IntegrationPage</div>
-  )
-}
+    <>
+      <Hero
+        title={HeroTitel}
+        content={HeroContent}
+        MyBackgroundImage={heroBg}
+      />
+      <RecentResult/>
+      <Breadcrumbs/>
+      <div className="container my-5">
+        <div className={styles.frameGroup}>
+          <div className={styles.parent}>
+            <div className={styles.div} style={{ fontSize: "36px" }}>
+              7+
+            </div>
+            <div className={styles.yearsOfAverageContainer}>
+              <p className={styles.yearsOfAverage}>
+                New Integration done last yer
+              </p>
+            </div>
+          </div>
+          <div className={styles.group}>
+            <div
+              className={styles.div1}
+              style={{ fontSize: "36px", top: "-1rem" }}
+            >
+              11Mn+
+            </div>
+            <div className={styles.clientsAcross5Container}>
+              <p className={styles.yearsOfAverage}>Lead Conversion</p>
+            </div>
+          </div>
+          <div className={styles.container}>
+            <div
+              className={styles.div2}
+              style={{ fontSize: "36px", left: "2rem" }}
+            >
+              100,000+
+            </div>
+            <div className={styles.projects} style={{ left: "1.2rem" }}>
+              Customer Win-Bank
+            </div>
+          </div>
+          <div className={styles.frameDiv}>
+            <div className={styles.div3} style={{ fontSize: "36px" }}>
+              13%-21%
+            </div>
+            <div
+              className={styles.provenTrackRecordContainer}
+              style={{ left: "0rem" }}
+            >
+              <p className={styles.yearsOfAverage}>
+                User Interaction Increased
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <ToolsSlider />
+      <ContactUsBar />
+      <ContactForm1 />
+      <TrustedByBrands />
+    </>
+  );
+};
 
-export default IntegrationPage
+export default IntegrationPage;
