620ddf44541cf3a1d07961c741b3036d813eb3c8
Shubham Sharma <Shubham101021@gmail.com>
2023-05-16 12:37:15 +0530

    some change

--- src/components/AccelerateYourGrowth.js
+++ src/components/AccelerateYourGrowth.js
@@ -1,6 +1,17 @@
 import styles from "./AccelerateYourGrowth.module.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const AccelerateYourGrowth = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    autoplay: true,
+    slidesToScroll: 1,
+  };
   return (
     <div className={styles.accelerateYourGrowth}>
       <div className={styles.frameParent}>
@@ -38,33 +49,53 @@ const AccelerateYourGrowth = () => {
           <button className={styles.frameButton}>
             <img className={styles.groupIcon} alt="" src="/group-358.svg" />
           </button>
+
           <div className={styles.a31483e59ee069da94c581Parent}>
-            <img
-              className={styles.a31483e59ee069da94c581Icon}
-              alt=""
-              src="/629a31483e59ee069da94c58-1@2x.png"
-            />
-            <img
-              className={styles.c471a6515b1e0ad75aa21Icon}
-              alt=""
-              src="/5841c471a6515b1e0ad75aa2-1@2x.png"
-            />
-            <img
-              className={styles.hubspotLogo1Icon}
-              alt=""
-              src="/hubspotlogo-1@2x.png"
-            />
-            <img className={styles.images1Icon} alt="" src="/images-1@2x.png" />
-            <img
-              className={styles.mailchimpLogoHorizontalBlacIcon}
-              alt=""
-              src="/mailchimp-logohorizontal-black-1@2x.png"
-            />
-            <img
-              className={styles.semrushLogo1Icon}
-              alt=""
-              src="/semrush-logo-1@2x.png"
-            />
+            {/* <Slider {...settings}> */}
+            <div className="px-2">
+              <img
+                className={styles.a31483e59ee069da94c581Icon}
+                alt=""
+                src="/629a31483e59ee069da94c58-1@2x.png"
+              />
+            </div>
+
+            <div>
+              <img
+                className={styles.c471a6515b1e0ad75aa21Icon}
+                alt=""
+                src="/5841c471a6515b1e0ad75aa2-1@2x.png"
+              />
+            </div>
+            <div>
+              <img
+                className={styles.hubspotLogo1Icon}
+                alt=""
+                src="/hubspotlogo-1@2x.png"
+              />
+            </div>
+            <div>
+              <img
+                className={styles.images1Icon}
+                alt=""
+                src="/images-1@2x.png"
+              />
+            </div>
+            <div>
+              <img
+                className={styles.mailchimpLogoHorizontalBlacIcon}
+                alt=""
+                src="/mailchimp-logohorizontal-black-1@2x.png"
+              />
+            </div>
+            <div>
+              <img
+                className={styles.semrushLogo1Icon}
+                alt=""
+                src="/semrush-logo-1@2x.png"
+              />
+            </div>
+          {/* </Slider> */}
           </div>
           <button className={styles.frameButton}>
             <img className={styles.groupIcon} alt="" src="/group-359.svg" />
