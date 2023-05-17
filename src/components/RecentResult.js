b65cdc8b2c5afc9f2e962af28b389831295bc26e
Shubham Sharma <Shubham101021@gmail.com>
2023-05-17 15:53:00 +0530

    chahner

--- src/components/RecentResult.js
+++ src/components/RecentResult.js
@@ -4,28 +4,24 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const RecentResult = () => {
-  const sliderData = [
-    " “170% increase in vertical revenue | Global Institute",
-    "“86% increase in organic traffic.” | Hotel Sector",
-    " “120% increase in PPC conversation rate.” | Higher Education",
-  ];
-  // const setting = {
-  //   dots: false,
-  //   infinite: true,
-  //   slidesToShow: 3,
-  //   slidesToScroll: 1,
-  //   initialSlide: 0,
-  //   adaptiveHeight: true,
-  //   autoplay: true,
-  //   speed: 2000,
-  //   autoplaySpeed: 1000,
-  //   cssEase: "linear",
-  // };
+  const setting = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    initialSlide: 1,
+    // adaptiveHeight: true,
+    autoplay: true,
+    speed: 5000,
+    autoplaySpeed: 3000,
+    arrow: false,
+    cssEase: "linear",
+  };
   return (
     <div className={styles.recentresult}>
       <div className={styles.recentResults}>Recent Results - </div>
       <div className={styles.increaseInVerticalRevenueParent}>
-        {/* <Slider {...setting}> */}
+        <Slider {...setting}>
           <div className={styles.increaseInVertical}>
             “170% increase in vertical revenue | Global Institute
           </div>
@@ -35,7 +31,7 @@ const RecentResult = () => {
           <div className={styles.increaseInVertical}>
             “120% increase in PPC conversation rate.” | Higher Education
           </div>
-        {/* </Slider> */}
+        </Slider>
       </div>
     </div>
   );
